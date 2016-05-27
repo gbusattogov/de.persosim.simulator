680f9122634e5ef300dbf811621d5d2ec081bdc3
Giorgio Busatto <Giorgio.Busatto@governikus.de>
2016-05-27 13:21:42 +0200

    Refactored and cleaned up code.

--- de.persosim.simulator.ui/src/de/persosim/simulator/ui/handlers/SelectPersoFromFileHandler.java
+++ de.persosim.simulator.ui/src/de/persosim/simulator/ui/handlers/SelectPersoFromFileHandler.java
@@ -1,7 +1,10 @@
 package de.persosim.simulator.ui.handlers;
 
+import java.io.File;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.widgets.Display;
@@ -11,7 +14,9 @@ import org.globaltester.logging.BasicLogger;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
 
@@ -39,59 +44,67 @@ public class SelectPersoFromFileHandler extends SelectPersoHandler {
 		}
 	}
 
-	public SelectPersoFromFileHandler()
-	{
-
+	public SelectPersoFromFileHandler() {
 	    final Bundle bundle = FrameworkUtil.getBundle(SelectPersoFromFileHandler.class);
-	    if (bundle == null)
-	    {
+	    if (bundle == null) {
 			BasicLogger.log(getClass(), "Cannot find bundle");
-	
+
 	    	return;
 	    }
-	
+
 	    final BundleContext ctx = Activator.getContext();
-	    if (ctx == null)
-	    {
+	    if (ctx == null) {
 			BasicLogger.log(getClass(), "Cannot find context");
-	
+
 	    	return;
 	    }
 
 	    EventHandler handler = new EventHandler() {
 	    	public void handleEvent(final Event event) {
-	    		final Object val = event.getProperty("PERSONALIZATION_FILE_PATH");
-	    		if (val == null || !(val instanceof String))
-	    		{
-	    			return;
-	    		}
+				final Object val = event.getProperty("PERSONALIZATION_FILE_PATH");
+				if (val == null || !(val instanceof String)) {
+					sendResponse("error");
 
-	    		final String filePath = (String) val;
-	    		final Display display = Display.getDefault();
-	    		if (display == null)
-	    		{
-	    			loadPersonalization(filePath);
+					return;
+				}
 
-	    			return;
-	    		}
+				final String filePath = (String) val;
+				if (!new File(filePath).isFile()) {
+					sendResponse("error");
 
-	    		if (display.getThread() == Thread.currentThread())
-	    		{
-	    			loadPersonalization(filePath);
-	    		}
-	    		else
-	    		{
-	    			display.syncExec(new Runnable() {
-	    				public void run() {
-	    					loadPersonalization(filePath);
-	    				}
-	    			});
-	    		}
-	    	}
-	    };
+					return;
+				}
 
-	    final Dictionary<String, String> properties = new Hashtable<String, String>();
-	    properties.put(EventConstants.EVENT_TOPIC, "repl_request/select-perso");
-	    ctx.registerService(EventHandler.class, handler, properties);
+				final Display display = Display.getDefault();
+				if (display == null) {
+					BasicLogger.log(getClass(), "Cannot find default display");
+					sendResponse("error");
+
+					return;
+				}
+
+				display.syncExec(new Runnable() {
+					public void run() {
+						loadPersonalization(filePath);
+
+						sendResponse("ok");
+					}
+				});
+			}
+		};
+
+		final Dictionary<String, String> properties = new Hashtable<String, String>();
+		properties.put(EventConstants.EVENT_TOPIC, "repl_request/select-perso");
+		ctx.registerService(EventHandler.class, handler, properties);
+	}
+
+	private void sendResponse(final String result) {
+		final Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("RESULT", result);
+
+		final BundleContext ctx = FrameworkUtil.getBundle(SelectPersoFromFileHandler.class).getBundleContext();
+		final ServiceReference<EventAdmin> ref = ctx.getServiceReference(EventAdmin.class);
+		final EventAdmin eventAdmin = ctx.getService(ref);
+		eventAdmin.sendEvent(new Event("repl_response/result", properties));
 	}
 }
